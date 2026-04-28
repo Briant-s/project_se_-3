3a0fa0687109e329ef4f22da84a8a594704f9bde
Giorrrr <giovanniyuvensetiawan@gmail.com>
2026-04-28 19:00:05 +0700

    add businessProfile models

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -12,3 +12,17 @@ class AmortEntry(BaseModel):
     
 class BusinessProfile(BaseModel):
     businessID: int 
+    created_at: str
+    businessName: str | None = None
+    businessAge: int | None = None
+    own_name: str | None = None
+    own_dob: str | None = None
+    businessLocation: str | None = None
+    businessBankAcc: str | None = None
+    businessSector: str | None = None
+    businessType: str | None = None
+    totalEmployees: int | None = None
+    storeType: str | None = None
+    monthlyAverageIncome: float | None = None
+    monthlyAverageProfitLoss: float | None = None
+    isOtherKredit: bool | None = None
