4ab4cf84202ddc1fd2a77870d5759d5aa0845be8
Briant-s <briantsandriano@gmail.com>
2026-04-28 22:15:50 +0700

    added user profile, removed some unused links

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -8,42 +8,19 @@ import {
   Stack,
   Text,
 } from "@mantine/core";
-import type { IconType } from "react-icons";
-import classes from "../App.module.css";
-import { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
 import type { NavItem } from "../types";
 import { NavLinkGroup } from "../lib";
 
 interface Props {
   mainNav: NavItem[];
-  supNav: NavItem[];
 }
 
-function Sidebar({ mainNav, supNav }: Props) {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const supLinks = supNav.map((nav) => (
-    <NavLink
-      className={classes.navLink}
-      bdrs={5}
-      key={nav.label}
-      active={location.pathname === nav.path}
-      label={nav.label}
-      leftSection={<nav.icon size={16} />}
-      onClick={() => {
-        navigate(nav.path);
-      }}
-    />
-  ));
-
+function Sidebar({ mainNav }: Props) {
   return (
     <Stack justify="space-between" h="calc(100vh - 80px)" gap="md">
-      
-      <Stack gap="sm" style={{ flex: 1, overflow: 'hidden' }}>
+      <Stack gap="sm" style={{ flex: 1, overflow: "hidden" }}>
         <Button fullWidth>Fill Out Business Quiz</Button>
-        
+
         <ScrollArea scrollbars="y" flex={1}>
           <Stack gap={4}>
             {mainNav.map((item) => (
@@ -51,11 +28,6 @@ function Sidebar({ mainNav, supNav }: Props) {
             ))}
           </Stack>
         </ScrollArea>
-      </Stack>
-
-      <Stack gap="xs" pb="md">
-        <Box>{supLinks}</Box>
-        <Box px={10} h={1} bg="gray.3"></Box>
       </Stack>
     </Stack>
   );
---
+++ frontend/src/components/UserProfile.tsx
@@ -0,0 +1,55 @@
+import { Avatar, Box, Button, Group, Menu, Stack, Text } from "@mantine/core";
+
+import {
+  HiLogout,
+  HiQuestionMarkCircle,
+  HiOutlineUser,
+  HiCog,
+  HiChevronDown,
+} from "react-icons/hi";
+
+interface Props {
+  user_email: string | null;
+  business_name: string | null;
+  handleSignOut: (e: any) => Promise<void>;
+}
+
+function UserProfile({ user_email, handleSignOut }: Props) {
+  return (
+    <>
+      <Menu width="target">
+        <Menu.Target>
+          <Button h="auto" p={5} size="sm" variant="subtle">
+            <Group w="100%">
+              <Avatar size="sm" src={null}></Avatar>
+              <Stack gap="0.1rem" flex={1}>
+                <Text size="xs" ta="left">
+                  {user_email}
+                </Text>
+                <Text size="xs" c="dimmed" ta="left">
+                  Business Name
+                </Text>
+              </Stack>
+              <HiChevronDown size={12} />
+            </Group>
+          </Button>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item leftSection={<HiOutlineUser />}>Profile</Menu.Item>
+          <Menu.Item leftSection={<HiCog />}>Settings</Menu.Item>
+          <Menu.Item leftSection={<HiQuestionMarkCircle />}>Help</Menu.Item>
+          <Menu.Divider />
+          <Menu.Item
+            leftSection={<HiLogout />}
+            c="red.5"
+            onClick={handleSignOut}
+          >
+            Logout
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    </>
+  );
+}
+
+export default UserProfile;
